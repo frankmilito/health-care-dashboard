108ca74eac7f6bbbad94f9d0edb35700ed1d7e5b
frankmilito <frankobidike20@gmail.com>
2024-10-18 12:09:30 +0100

    setup prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div >
-     Home screen
+    <div className="min-h-screen">
+      <Navbar/>
+    <div className="flex h-full">
+      <h1 className="flex-1 bg-black">left sidebar</h1>
+      <h1 className="flex-1 bg-orange-200">main</h1>
+      <h1 className="flex-1 bg-blue-50">right sidebar</h1>
+    </div>
     </div>
   );
 }
